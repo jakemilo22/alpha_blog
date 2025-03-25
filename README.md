38901df05fd871a556bc69ae9f0292489c2fac16
Jake Milo <jake.milo@javln.com>
2025-03-25 13:37:40 +1300

    Added validations to articles table

--- README.md
+++ README.md
@@ -23,3 +23,4 @@ Things you may want to cover:
 
 ** Home and About Pages added
 ** Added articles table
+** Add validations to articles table
---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+# this model can now communicate to the database via rails console
+class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+end
